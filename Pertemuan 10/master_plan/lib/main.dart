f57b172d3b69916ed064d056363a0539abcc926c
Dhnillhaq <yakulhaq@gmail.com>
2025-12-22 12:39:03 +0700

    feat: pertemuan 10

---
+++ Pertemuan 10/master_plan/lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import './views/plan_creator_screen.dart';
+
+import './provider/plan_provider.dart';
+import './models/data_layer.dart';
+
+void main() => runApp(MasterPlanApp());
+
+class MasterPlanApp extends StatelessWidget {
+  const MasterPlanApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.purple),
+      home: PlanProvider(
+        notifier: ValueNotifier<List<Plan>>(const []),
+        child: MaterialApp(
+          title: 'State management app',
+          theme: ThemeData(primarySwatch: Colors.blue),
+          home: const PlanCreatorScreen(),
+        ),
+      ),
+    );
+  }
+}
